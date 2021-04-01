7dd788a970c6999cd9126b47f519097a8fd327f3
brian-gpu <79373206+brian-gpu@users.noreply.github.com>
2021-03-31 21:18:26 -0400

    update visualization logger

--- references/visualize_data.py
+++ references/visualize_data.py
@@ -4,14 +4,12 @@ import numpy as np
 import matplotlib as plt
 import logging
 
-plt.rc('figure', figsize=(30,16))
-
 def setup_sns(logger=logging):
     try:
         sns.set(rc={'axes.facecolor':'white', 'figure.facecolor':'white'})
         sns.despine()
     except Exception as e:
-        logging.info(f'Could not setup seaborn defaults {e}')
+        logger.info(f'Could not setup seaborn defaults {e}')
 
 def setup_labels(ax, title, x_label, y_label, logger=logging):
     try:
@@ -19,14 +17,14 @@ def setup_labels(ax, title, x_label, y_label, logger=logging):
         ax.set_xlabel(x_label, color='gray')
         ax.set_ylabel(y_label, color='gray')
     except Exception as e:
-        logging.info(f'Could not set axis labels {e}')
+        logger.info(f'Could not set axis labels {e}')
 
 def setup_grid(ax, axis, logger=logging):
     try:
         ax.tick_params(labelcolor='gray', axis=axis)
         ax.grid(color='gray', axis=axis, linestyle='dashed', linewidth=.5)
     except Exception as e:
-        logging.info(f'Could not set axis grid defaults {e}')
+        logger.info(f'Could not set axis grid defaults {e}')
 
 
 def sort_data(data, sort_by='', asc=None, logger=logging):
@@ -34,7 +32,7 @@ def sort_data(data, sort_by='', asc=None, logger=logging):
         try:
             data = data.sort_values(by=sort_by, ascending=asc)
         except Exception as e:
-            logging.info(f'Could not sort data {e}')
+            logger.info(f'Could not sort data {e}')
 
     return data
 
@@ -46,7 +44,7 @@ def create_barplot(data, title, x_label, y_label, asc=None, logger=logging):
     try:
         ax = sns.barplot(x='data', y='label', data=data)
     except Exception as e:
-        logging.info(f'Could not create barplot {e}')
+        logger.info(f'Could not create barplot {e}')
 
     setup_labels(ax, title, x_label, y_label, logger)
     setup_grid(ax, 'x', logger)
@@ -59,7 +57,7 @@ def create_stackplot(data, title, x_label, y_label, logger=logging):
     try:
         ax = data.set_index('Area').T.plot(kind='bar', stacked=True, color=sns.color_palette('Paired'))
     except Exception as e:
-        logging.info(f'Could not create stackpot {e}')
+        logger.info(f'Could not create stackpot {e}')
 
     setup_labels(ax, title, x_label, y_label, logger)
     setup_grid(ax, 'y', logger)
@@ -72,7 +70,7 @@ def create_lineplot(data, title, x_label, y_label, logger=logging):
     try:
         ax = data.set_index('Area').T.plot(kind='line', linewidth=1, marker='o', color=sns.color_palette('bright'))
     except Exception as e:
-        logging.info(f'Could not create lineplot {e}')
+        logger.info(f'Could not create lineplot {e}')
 
     setup_labels(ax, title, x_label, y_label, logger)
     setup_grid(ax, 'y', logger)
@@ -85,13 +83,17 @@ def save_plot(plot, destination='plot.png', logger=logging):
     try:
         image = plot.get_figure()
     except Exception as e:
-        logging.info(f'Could not retrieve figure {e}')
+        logger.info(f'Could not retrieve figure {e}')
     
     try:
         image.savefig(destination, bbox_inches='tight')
     except Exception as e:
-        logging.info(f'Could not save image {e}')
+        logger.info(f'Could not save image {e}')
 
+
+# ----------Example Usage----------
+# Set the figure size
+plt.rc('figure', figsize=(30,16))
 
 # Set dataframe and labels
 x_label = 'Sales'
