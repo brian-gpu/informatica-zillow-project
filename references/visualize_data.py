cfe208599126a34944e0d3d9122c928300a5dd99
brian-gpu <79373206+brian-gpu@users.noreply.github.com>
2021-04-01 16:38:55 -0400

    Add docstrings to functions

--- references/visualize_data.py
+++ references/visualize_data.py
@@ -5,6 +5,14 @@ import matplotlib as plt
 import logging
 
 def setup_sns(logger=logging):
+    '''
+    Helper function to setup seaborn defaults
+    Parameters:
+        logger (optional): the logger to forward logs
+    Returns:
+        nothing
+    '''
+    
     try:
         sns.set(rc={'axes.facecolor':'white', 'figure.facecolor':'white'})
         sns.despine()
@@ -12,6 +20,18 @@ def setup_sns(logger=logging):
         logger.info(f'Could not setup seaborn defaults {e}')
 
 def setup_labels(ax, title, x_label, y_label, logger=logging):
+    '''
+    Helper function to set labels on matplotlib axis object 
+    Parameters:
+        ax (matplotlib axis object): the plot to label
+        title (str): the title of the plot
+        x_label (str): the label for the x-axis
+        y_label (str): the label for the y-axis
+        logger (optional): the logger to forward logs
+    Returns:
+        nothing
+    '''
+
     try:
         ax.set_title(title, size=16)
         ax.set_xlabel(x_label, color='gray')
@@ -20,6 +40,16 @@ def setup_labels(ax, title, x_label, y_label, logger=logging):
         logger.info(f'Could not set axis labels {e}')
 
 def setup_grid(ax, axis, logger=logging):
+    '''
+    Helper function to set the ticks and custom axis aesthetic
+    Parameters:
+        data (matplotlib axis object): the plot to clear grid
+        axis (str): the axis to grey out
+        logger (optional): the logger to forward logs
+    Returns:
+        nothing
+    '''
+
     try:
         ax.tick_params(labelcolor='gray', axis=axis)
         ax.grid(color='gray', axis=axis, linestyle='dashed', linewidth=.5)
@@ -28,6 +58,17 @@ def setup_grid(ax, axis, logger=logging):
 
 
 def sort_data(data, sort_by='', asc=None, logger=logging):
+    '''
+    Helper function to sort data in dataframe 
+    Parameters:
+        data (dataframe): the data to sort
+        sort_by (str): the name of the column to sort by
+        asc (optional: bool | None): sort the order descending/ascending (False/True)
+        logger (optional): the logger to forward logs
+    Returns:
+        nothing
+    '''
+
     if(asc != None):
         try:
             data = data.sort_values(by=sort_by, ascending=asc)
@@ -37,6 +78,20 @@ def sort_data(data, sort_by='', asc=None, logger=logging):
     return data
 
 def create_barplot(data, title, x_label, y_label, asc=None, logger=logging):
+    '''
+    Creates a matplotlib axis object for a seaborn barplot
+        Note: must contain column names 'data' and 'label'
+    Parameters:
+        data (dataframe): the data to plot
+        title (str): the title of the plot
+        x_label (str): the label for the x-axis
+        y_label (str): the label for the y-axis
+        asc (optional: bool | None): sort the order descending/ascending (False/True)
+        logger (optional): the logger to forward logs
+    Returns:
+        ax (matplotlib axis object): the plot
+    '''
+
     ax = None
     
     data = sort_data(data, sort_by='data', asc=asc)
@@ -52,6 +107,19 @@ def create_barplot(data, title, x_label, y_label, asc=None, logger=logging):
     return ax
 
 def create_stackplot(data, title, x_label, y_label, logger=logging):
+    '''
+    Creates a matplotlib axis object for a pandas stackplot
+        Note: must contain column name 'Area'
+    Parameters:
+        data (dataframe): the data to plot
+        title (str): the title of the plot
+        x_label (str): the label for the x-axis
+        y_label (str): the label for the y-axis
+        logger (optional): the logger to forward logs
+    Returns:
+        ax (matplotlib axis object): the plot
+    '''
+
     ax = None
 
     try:
@@ -65,6 +133,19 @@ def create_stackplot(data, title, x_label, y_label, logger=logging):
     return ax
 
 def create_lineplot(data, title, x_label, y_label, logger=logging):
+    '''
+    Creates a matplotlib axis object for a pandas lineplot
+        Note: must contain column name 'Area'
+    Parameters:
+        data (dataframe): the data to plot
+        title (str): the title of the plot
+        x_label (str): the label for the x-axis
+        y_label (str): the label for the y-axis
+        logger (optional): the logger to forward logs
+    Returns:
+        ax (matplotlib axis object): the plot
+    '''
+
     ax = None
     
     try:
@@ -78,6 +159,17 @@ def create_lineplot(data, title, x_label, y_label, logger=logging):
     return ax
 
 def save_plot(plot, destination='plot.png', logger=logging):
+    '''
+    Saves a matplotlib axis object as a .png a destination
+    Parameters:
+        plot (matplotlib axis object): the plot to save
+        destination (optional: str): the name and/or path to save the file (e.g. /temp/file.png)
+            -- note: default behavior creates a file 'plot.png' in the working directory
+        logger (optional): the logger to forward logs
+    Returns:
+        nothing
+    '''
+
     setup_sns(logger)
 
     try:
